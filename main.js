8974f73821694eebbabbc2d7edcc5923c5aca1fe
“onble” <62949816+onble@users.noreply.github.com>
2023-07-01 11:01:03 +0800

    完成了首页功能的开发

--- main.js
+++ main.js
@@ -1,25 +1,57 @@
-
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+
+// 导入网络请求的包
+import {
+	$http
+} from '@escook/request-miniprogram'
+
+uni.$http = $http
+
+// 请求的根路径
+$http.baseUrl = 'https://api-hmugo-web.itheima.net'
+
+// 配置请求拦截器，增加提示
+$http.beforeRequest = function(options) {
+	uni.showLoading({
+		title: '数据加载中...'
+	})
+}
+
+// 配置响应拦截器，关闭提示
+$http.afterRequest = function() {
+	uni.hideLoading()
+}
+
+// 封装弹框的方法
+uni.$showMsg = function(title = '数据请求失败!', duration = 1500) {
+	uni.showToast({
+		title,
+		duration,
+		icon: 'none'
+	})
+}
 
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App
+	...App
 })
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+	createSSRApp
+} from 'vue'
 import App from './App.vue'
 export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
+	const app = createSSRApp(App)
+	return {
+		app
+	}
 }
 // #endif
